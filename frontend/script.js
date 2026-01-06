702aa769df2eb004a3a7a85dc680a3c533fb2eba
Abymael-hd <abymael202@gmail.com>
2026-01-06 13:06:01 -0300

    Atualiza URL do backend para produção

--- frontend/script.js
+++ frontend/script.js
@@ -124,7 +124,7 @@ window.addEventListener('load', revealOnScroll);
 
     }
 
-fetch('http://localhost:3000/contato', {
+fetch('https://curriculo-backend-h8a5.onrender.com', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json'
