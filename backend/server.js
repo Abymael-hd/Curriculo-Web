48051a52674dab483b6a9749c2c39fe90dff5952
Abymael-hd <abymael202@gmail.com>
2026-01-07 23:46:55 -0300

    Ajusta remetente verificado no Resend

--- backend/server.js
+++ backend/server.js
@@ -30,8 +30,8 @@ app.post("/contato", async (req, res) => {
 
   try {
     await resend.emails.send({
-      from: "Currículo Web <onboarding@resend.dev>",
-      to: ["SEU_EMAIL@gmail.com"], // <-- coloque seu email aqui
+      from: "Currículo Web <abymael202@gmail.com>",
+      to: ["abymael202@gmail.com"], // <-- coloque seu email aqui
       reply_to: email,
       subject: "Contato - Currículo Web",
       html: `
